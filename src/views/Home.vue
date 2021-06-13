7180c725da1d516734344b62e856509c5dc9f414
Vladimir Shefer <quameu@gmail.com>
2021-06-13 17:50:20 +0300

    Calendar view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    {{ phone }}
+    <calendar-heatmap :values="stats" :end-date="new Date()" tooltip-unit="рублей"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {Component, Vue} from 'vue-property-decorator';
+import lkdrApiClient from "@/apiclients/lkdr";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
+
+@Component<Home>({
+  mounted: function () {
+    if (lkdrApiClient.getAuth()) {
+      this.loadPhone()
+      this.loadStats()
+    }
+
+    lkdrApiClient.onAuthStateChanged(auth => {
+      if (!auth) return;
+      this.loadPhone();
+      this.loadStats();
+    });
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  phone: string | null = null;
+  stats: any = [];
+
+  loadPhone() {
+    lkdrApiClient.getAxios().get("/api/v1/user/profile")
+      .then(response => response.data?.user?.taxpayerPerson?.phone)
+      .then(phone => this.phone = phone)
+  }
+
+  loadStats() {
+    let axios1 = lkdrApiClient.getAxios();
+    axios1.post("/api/v1/receipt", {
+      limit: 1000,
+      offset: 0,
+      dateFrom: null,
+      dateTo: null,
+      orderBy: "RECEIVE_DATE:DESC",
+      inn: null
+    })
+      .then(response => response.data)
+      .then(data => data?.receipts)
+      .then((receipts: any[]) => receipts.map((receipt: any) => {
+        return {date: receipt.createdDate.substring(0, 10), sum: parseFloat(receipt.totalSum)}
+      }))
+      .then((receipts: any[]) => {
+        let result: any = {};
+        for (let receipt of receipts) {
+          let date = receipt.date;
+          if (!result[date]) {
+            result[date] = 0.0
+          }
+          result[date] += receipt.sum
+        }
+        return result;
+      })
+      .then((stats: any) => {
+        let data = []
+        for (let date in stats) {
+          // noinspection JSUnfilteredForInLoop
+          data.push({date: date, count: Math.floor(stats[date])})
+        }
+        return data
+      })
+      .then(stats => this.stats = stats)
+  }
+
+}
 </script>
