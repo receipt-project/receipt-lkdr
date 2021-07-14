d085324dff8320940fa91873397174cf87d5a5c2
vladimirshefer <quameu@gmail.com>
2021-07-14 21:16:19 +0000

    Deploying to gh-pages from @ receipt-project/receipt-lkdr@409e82a7f8ede1c93dbf412cd461142f1ed39160 🚀

---
+++ precache-manifest.ec53f062515eca9eb35095ce030aff7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e811b74e4dc3836b142",
+    "url": "/css/app.ffea60d3.css"
+  },
+  {
+    "revision": "81dd139cfdaed22b784c",
+    "url": "/css/chunk-vendors.2c7e0f32.css"
+  },
+  {
+    "revision": "4ece4b6f38d9dad91e5bce25d1817049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "119cb14c47d94dc898e8",
+    "url": "/js/about.047c8b02.js"
+  },
+  {
+    "revision": "4e811b74e4dc3836b142",
+    "url": "/js/app.a0edc1d2.js"
+  },
+  {
+    "revision": "81dd139cfdaed22b784c",
+    "url": "/js/chunk-vendors.19a8ef58.js"
+  },
+  {
+    "revision": "080c830a9ec412211c4a32a02b17735b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
