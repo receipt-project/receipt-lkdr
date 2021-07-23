0abd57b60265c6e056b72de4714522b9ed878f2d
Vladimir Shefer <quameu@gmail.com>
2021-07-24 00:50:55 +0300

    Recreate DateSelector

--- src/components/DatesSelector.vue
+++ src/components/DatesSelector.vue
@@ -1,15 +1,6 @@
 <template>
-  <div>
-    <div>
-      <button v-for="year in years" :key="year" @click="clickYear(year)">
-        {{ year }}
-      </button>
-    </div>
-    <div>
-      <button v-for="month in months" :key="month" @click="clickMonth(month)">
-        {{ String(month).padStart(2, "0") }}
-      </button>
-    </div>
+  <div class="row">
+    <DatePicker v-model="datesRange" range></DatePicker>
   </div>
 </template>
 
@@ -17,30 +8,21 @@
 import {Component, Vue} from "vue-property-decorator";
 import dayjs from "dayjs";
 
-@Component<DatesSelector>({})
+@Component<DatesSelector>({
+  watch: {
+    datesRange(value) {
+      if (!value) return;
+      this.dayFrom = dayjs(value[0]);
+      this.dayTo = dayjs(value[1]);
+      this.emitDates()
+    }
+  }
+})
 export default class DatesSelector extends Vue {
-  years = [2018, 2019, 2020, 2021]
-  months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  datesRange: string | null = null;
 
-  selectedYear = 2021
-  selectedMonth = 1;
-
-  dayFrom: dayjs.Dayjs = dayjs().year(this.selectedYear).startOf("year")
-  dayTo: dayjs.Dayjs = dayjs().year(this.selectedYear).endOf("year")
-
-  clickMonth(month: number) {
-    this.selectedMonth = month
-    this.dayFrom = dayjs().year(this.selectedYear).month(this.selectedMonth - 1).startOf("month")
-    this.dayTo = dayjs().year(this.selectedYear).month(this.selectedMonth - 1).endOf("month")
-    this.emitDates()
-  }
-
-  clickYear(year: number) {
-    this.selectedYear = year
-    this.dayFrom = dayjs().year(this.selectedYear).startOf("year")
-    this.dayTo = dayjs().year(this.selectedYear).endOf("year")
-    this.emitDates()
-  }
+  dayFrom: dayjs.Dayjs = dayjs().year(dayjs().year() - 1)
+  dayTo: dayjs.Dayjs = dayjs()
 
   private emitDates() {
     this.$emit("changed", {
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,23 @@ import store from './store'
 // @ts-ignore
 import VueCalendarHeatmap from 'vue-calendar-heatmap'
 import VueApexCharts from 'vue-apexcharts'
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+import weekYear from "dayjs/plugin/weekYear";
+import weekOfYear from "dayjs/plugin/weekOfYear"; // dependent on weekOfYear plugin
+
+import 'vue2-datepicker/index.css';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import DatePicker from 'vue2-datepicker';
+
+dayjs.extend(weekday)
+dayjs.extend(weekOfYear)
+dayjs.extend(weekYear)
 
 Vue.use(VueCalendarHeatmap)
 Vue.use(VueApexCharts)
+Vue.use(DatePicker)
 
 Vue.component('apexchart', VueApexCharts)
 
