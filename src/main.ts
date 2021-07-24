189ed35acb2317867145f8a611f12ae929dc3d1b
Vladimir Shefer <quameu@gmail.com>
2021-07-24 22:04:46 +0300

    Install Vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from '@/plugins/vuetify'
 
 require("@/plugins/vue-apexcharts")
 require("@/plugins/dayjs")
@@ -14,5 +15,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
